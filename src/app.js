3b01f48d2f69dbdbfd430b7d9b8e8ad5affbd716
Daniel <dgl.daniel@outlook.com.br>
2020-04-20 16:51:12 -0300

    Main tests passed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,90 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newRepository);
+
+  return response.json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const checkUUID = isUuid(id);
+
+  if (!checkUUID) {
+    return response.status(400).json({ message: "ID is not uuid" });
+  }
+
+  const indexRepository = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (indexRepository < 0) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+
+  repositories[indexRepository] = { title, url, techs };
+
+  return response.json(repositories[indexRepository]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const checkUUID = isUuid(id);
+
+  if (!checkUUID) {
+    return response.status(400).json({ message: "ID is not uuid" });
+  }
+
+  const indexRepository = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (indexRepository < 0) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+
+  repositories.splice(indexRepository, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const checkUUID = isUuid(id);
+
+  if (!checkUUID) {
+    return response.status(400).json({ message: "ID is not uuid" });
+  }
+
+  const indexRepository = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (indexRepository < 0) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+
+  repositories[indexRepository] = repositories[indexRepository].like++;
+
+  return response.json(repositories[indexRepository]);
 });
 
 module.exports = app;
